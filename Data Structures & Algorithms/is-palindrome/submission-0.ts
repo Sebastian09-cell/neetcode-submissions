ca13c28ec8c783003d1881a3a255118aa1805826
Sebastian Torres Delgado <152095820+Sebastian09-cell@users.noreply.github.com>
2026-09-10 15:33:23 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+        
+        let i = 0;
+        let j = s.length - 1;
+
+        while (i < j) {
+            if (s[i] === s[j]) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
