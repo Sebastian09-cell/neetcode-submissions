28c63cf804d67527cfbc58686daba4eebd2010e7
Sebastian Torres Delgado <152095820+Sebastian09-cell@users.noreply.github.com>
2026-09-10 15:42:40 -0500

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let i = 0;
+        let j = numbers.length - 1;
+
+        while (i < j) {
+            let resultado = numbers[i] + numbers[j];
+            if (resultado == target ) {
+                return [i + 1, j + 1];
+            } else if (resultado < target){
+                i++
+            }else{
+                j--
+            }
+          
+        }
+        return [];
+    }
+}
